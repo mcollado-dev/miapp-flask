ae8557f287b15fbebd0323d5b1d5fc906184f5ff
mcollado-dev <mcollado@fidesol.org>
2025-10-31 12:04:55 +0100

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,9 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    rol = db.Column(db.String(50), nullable=False, default='Lector')
 
     def __repr__(self):
         return f'<Usuario {self.nombre}>'
+
 
